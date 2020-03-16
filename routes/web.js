019e14ebe5e39ee51dfc2156bc49aaaec8328570
adevr <alex.cst.reis@gmail.com>
2020-03-16 22:37:17 +0000

    chore(styles): update platform styles

--- routes/web.js
+++ routes/web.js
@@ -1,4 +1,7 @@
-"use strict";
+const geocode = require("./utils/geocode");
+const forecast = require("./utils/forecast");
+
+
 
 module.exports = {
     about: (req, res) => {
@@ -20,9 +23,28 @@ module.exports = {
         })
     },
     weather: (req, res) => {
-        res.send({
-            forecast: "It is snowing",
-            location: "Aveiro"
+        if (!req.query.address) {
+            return res.send({
+                error: 'You must provide an address!'
+            })
+        }
+
+        geocode(req.query.address, (error, { latitude, longitude, location } = {}) => {
+            if (error) {
+                return res.send({ error })
+            }
+
+            forecast(latitude, longitude, (error, forecastData) => {
+                if (error) {
+                    return res.send({ error })
+                }
+
+                res.send({
+                    forecast: forecastData,
+                    location,
+                    address: req.query.address
+                })
+            })
         })
     },
     p404: (req, res) => {
