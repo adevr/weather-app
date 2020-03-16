10ca4a254171518fa47b7d25e50cce75e93f8778
adevr <alex.cst.reis@gmail.com>
2020-03-16 20:58:34 +0000

    feat: add app.js file

---
+++ app.js
@@ -0,0 +1,28 @@
+const path = require("path");
+const express = require("express");
+const webRoutes = require("./routes/web.js");
+const hbs= require("hbs");
+
+const app = express();
+
+// path configuration for express
+const publicDirectory = path.join(__dirname, "/public");
+const viewsPath = path.join(__dirname, "/templates/views");
+const partialsPath = path.join(__dirname, "/templates/partials");
+
+//handlebars configuration
+app.set("view engine", "hbs");
+app.set("views", viewsPath);
+hbs.registerPartials(partialsPath);
+
+app.use(express.static(publicDirectory));
+
+app.get("/", webRoutes.root);
+app.get("/about", webRoutes.about);
+app.get("/help", webRoutes.help);
+app.get("/weather", webRoutes.weather);
+app.get("*", webRoutes.p404);
+
+app.listen(3000, () => {
+    console.log("Server is up on port 3000")
+});
